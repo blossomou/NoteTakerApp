115a9841d1403154024e2f41ee74ce89c48431fa
QingOu <qingqingou@gmail.com>
2022-02-11 17:37:15 -0500

    use fs to read the json file

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import express from 'express';
+import { readFile } from 'fs';
 import path from 'path';
 
-import notes from './database/notes.json';
+//import notes from './database/notes.json';
 
 //another way of importing
 // const express = require("express");
@@ -23,6 +24,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //app.use(express.json())
 //app.use(express.urlencoded({extended: true}))
 //
+
+const readNotes = () => {
+  return new Promise((resolve, reject) => {
+    readFile("./database/notes.json", "utf8", function (err, data) {
+      if (err) {
+        console.log("err", err);
+        resolve([]);
+      }
+
+      resolve(JSON.parse(data));
+    });
+  });
+};
 
 app.use(express.static("public"));
 
@@ -51,18 +65,18 @@ function findById(id, notesArray) {
 
 //Get
 app.get("/api/notes", (req, res) => {
-  let results = notes;
-
-  res.json(results);
+  readNotes().then((result) => res.json(result));
 });
 
 app.get("/api/notes/:id", (req, res) => {
-  const result = findById(req.params.id, notes);
-  if (result) {
-    res.json(result);
-  } else {
-    res.send(404);
-  }
+  readNotes().then((notes) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
+  });
 });
 
 app.listen(PORT, () => {
